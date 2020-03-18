a29adfad5030a8b88774d446f8a4375f7ad5a9a5
lolitosalva <54906081+lolitosalva@users.noreply.github.com>
2020-03-17 22:16:33 -0600

    Add files via upload

---
+++ js/customJS.js
@@ -0,0 +1,38 @@
+// TESTING
+// INGREDIENT FUNCTION
+function addIngredient() {
+    const div = document.createElement('div');
+    div.className = "mb-3";
+    div.innerHTML = `
+      <input type="text" class="form-control mb-3" />
+      <button type="button" class="btn btn-primary mr-3" onclick="addIngredient()">Add Ingredient</button>
+      <button type="button" class="btn btn-danger" onclick="removeIngredient(this)">Remove Ingredient</button>
+    `;
+    document.getElementById('ingredientDiv').appendChild(div);
+  }
+  
+  function removeIngredient(input) {
+    document.getElementById('ingredientDiv').removeChild(input.parentNode);
+  }
+// INSTRUCTION FUNCTION
+  function addInstruction() {
+    const div = document.createElement('div');
+    div.className = "mb-3";
+    div.innerHTML = `
+      <input type="text" class="form-control mb-3" />
+      <button type="button" class="btn btn-primary mr-3" onclick="addInstruction()">Add Instruction</button>
+      <button type="button" class="btn btn-danger" onclick="removeInstruction(this)">Remove Instruction</button>
+    `;
+    document.getElementById('instructionDiv').appendChild(div);
+  }
+
+  function removeInstruction(input) {
+    document.getElementById('instructionDiv').removeChild(input.parentNode);
+  }
+
+
+// CURRENTLY PRESSING BUTTON RESETS THE PAGE AND RETURNS TO THE TOP
+document.getElementByElement("button").addEventListener("click", function(event){
+    event.preventDefault();
+    handleFireButton();
+});
