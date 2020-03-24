bf0b341cb0a9292e1579a8332f73aeeb2f65e21f
lolitosalva <54906081+lolitosalva@users.noreply.github.com>
2020-03-23 20:26:07 -0600

    Update customJS.js
    Added and remove a style attribute to the created div element.

--- js/customJS.js
+++ js/customJS.js
@@ -3,7 +3,6 @@
 function addIngredient() {
     const div = document.createElement('div');
     div.className = "mb-3";
-    div.setAttribute("style", "transition: 0.3s");
     div.innerHTML = `
       <input type="text" class="form-control mb-3" />
       <button type="button" class="btn btn-primary mr-3" onclick="addIngredient()">Add Ingredient</button>
@@ -19,7 +18,6 @@ function addIngredient() {
   function addInstruction() {
     const div = document.createElement('div');
     div.className = "mb-3";
-    div.setAttribute("style", "transition: 0.3s");
     div.innerHTML = `
       <input type="text" class="form-control mb-3" />
       <button type="button" class="btn btn-primary mr-3" onclick="addInstruction()">Add Instruction</button>
