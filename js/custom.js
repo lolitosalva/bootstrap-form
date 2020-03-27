667c7c0763f7cbf6d9cf57f2716c721265eeb96b
lolitosalva <54906081+lolitosalva@users.noreply.github.com>
2020-03-27 00:01:39 -0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-function addIngredient() {
+/** function addIngredient() {
     const div = document.createElement('div');
     div.className = "mb-3 input-group removeBtn";
     div.innerHTML = `
@@ -16,4 +16,21 @@ function removeIngredient(input) {
 document.getElementsByTagName("button").addEventListener("click", function(){
     event.preventDefault();
     handleFireButton();
-});
+}); **/
+
+function addIngredient() {
+    const div = document.createElement('div');
+    div.className = "mb-3 input-group removeBtn";
+    div.innerHTML = `
+        <input type="text" class="form-control" />
+        <button type="button" class="btn btn-outline-danger input-group-prepend" onclick="removeIngredient(this)">Remove</button>
+    `;
+    document.getElementById('ingredientDiv').appendChild(div);
+    event.preventDefault();
+    handleFireButton();
+  }
+  
+function removeIngredient(input) {
+    document.querySelector('.removeBtn').remove(this);
+}
+
