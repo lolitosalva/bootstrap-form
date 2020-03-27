5aeed3d34c240de95609592895fdee9a2d0bdab3
lolitosalva <54906081+lolitosalva@users.noreply.github.com>
2020-03-26 22:06:35 -0600

    Changed JS
    Same functionality but because of the revamp, new bits of the code has been changed. Still Vanilla JS.

--- js/customJS.js
+++ js/customJS.js
@@ -1,38 +1,15 @@
-// TESTING
-// INGREDIENT FUNCTION
 function addIngredient() {
     const div = document.createElement('div');
-    div.className = "mb-3";
+    div.className = "mb-3 input-group removeBtn";
     div.innerHTML = `
-      <input type="text" class="form-control mb-3" />
-      <button type="button" class="btn btn-primary mr-3" onclick="addIngredient()">Add Ingredient</button>
-      <button type="button" class="btn btn-danger" onclick="removeIngredient(this)">Remove Ingredient</button>
+        <input type="text" class="form-control" />
+        <button type="button" class="btn btn-outline-danger input-group-prepend" onclick="removeIngredient(this)">Remove</button>
     `;
     document.getElementById('ingredientDiv').appendChild(div);
-  }
-  
-  function removeIngredient(input) {
-    document.getElementById('ingredientDiv').removeChild(input.parentNode);
-  }
-// INSTRUCTION FUNCTION
-  function addInstruction() {
-    const div = document.createElement('div');
-    div.className = "mb-3";
-    div.innerHTML = `
-      <input type="text" class="form-control mb-3" />
-      <button type="button" class="btn btn-primary mr-3" onclick="addInstruction()">Add Instruction</button>
-      <button type="button" class="btn btn-danger" onclick="removeInstruction(this)">Remove Instruction</button>
-    `;
-    document.getElementById('instructionDiv').appendChild(div);
-  }
-
-  function removeInstruction(input) {
-    document.getElementById('instructionDiv').removeChild(input.parentNode);
-  }
-
-
-// CURRENTLY PRESSING BUTTON RESETS THE PAGE AND RETURNS TO THE TOP
-document.getElementByElement("button").addEventListener("click", function(event){
     event.preventDefault();
     handleFireButton();
-});
+  }
+  
+function removeIngredient(input) {
+    document.querySelector('.removeBtn').remove(this);
+}
